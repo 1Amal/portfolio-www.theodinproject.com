6c94dccc05e42c44d58bcc078eefee77b3e12660
Amal <12756149+1Amal@users.noreply.github.com>
2022-12-27 11:59:38 +1100

    Initial work on06-flex-layout

---
+++ foundations-path/js-foundations/javascript.js
@@ -0,0 +1,11 @@
+    // Your JavaScript goes here!
+    "use strict";
+    let message='Hello World!';
+    let Message2='Hello Amal!';
+    const message3="30203042432";
+    message3="33";
+    let 我="Chinese"
+     console.log(message);
+     console.log(message3);
+    alert(Message2);
+    alert(我);
