72adbeef3b33d17d69290688b125e51ff901bf7d
Amal K <12756149+1Amal@users.noreply.github.com>
2023-04-26 08:25:11 +1000

    Added logic for round counter

--- foundations-path/js-foundations/Project: Rock Paper Scissors/logic.js
+++ foundations-path/js-foundations/Project: Rock Paper Scissors/logic.js
@@ -95,6 +95,7 @@ End of Psuedo code for playRound
 // console.log(computerSelection());
 
 function playRound(playerSelection, computerSelection) {
+currentRound+=1;  // This code will add 1 to the round so it can be limited to 5 rounds  
 
 console.log("Player selection: " + playerSelection)
 console.log("Computer Selection: "+ computerSelection)
@@ -112,12 +113,15 @@ if (playerSelection=="rock") // Following logic will decide what the outcome is 
         
         else if (computerSelection=="paper")
         {
-            return console.log("You selected: " + playerSelection + " and Computer selected: " + computerSelection + " So Computer wins !")
+            console.log("You selected: " + playerSelection + " and Computer selected: " + computerSelection + " So Computer wins !")
+            
+            
         }
 
         else (computerSelection=="scissors")
         {
-            return console.log("You selected: " + playerSelection + " and Computer selected: " + computerSelection + " So Player wins !")
+            console.log("You selected: " + playerSelection + " and Computer selected: " + computerSelection + " So Player wins !")
+            return playerScore+=1;
         }
         
     }    
@@ -159,7 +163,6 @@ else (playerSelection=="scissors")
     }
 
 }
-        
 }
 
 
@@ -174,20 +177,21 @@ else (playerSelection=="scissors")
  keeps score and reports a winner or loser at the end.
 //     You have not officially learned how to “loop” over code to repeat function calls… 
 if you already know about loops from somewhere else (or if you feel like doing some more learning) feel free to use them. 
-If not, don’t worry! Just call your playRound function 5 times in a row. Loops are covered in the next lesson.
+If not, don’t worry! let currentRound=1;Just call your playRound function 5 times in a row. Loops are covered in the next lesson.
 //     At this point you should be using console.log() to display the results of each round and the winner at the end.
 //     Use prompt() to get input from the user. Read the docs here if you need to.
 //     Feel free to re-work your previous functions if you need to. Specifically, you might want to change the return 
 value to something more useful.
 //     Feel free to create more “helper” functions if you think it would be useful.
 */
-
+let currentRound=0;
+let playerScore=0;
+let computerScore=0;
 function game()
 {
     playRound (playerAnswer(),getComputerChoice()); // Return function out put for player answer computer answer to playRound function
-    let playerScore=0;
-    let computerScore=0;
-    let currentRound=1;
+
+    
     
     console.log("Current round is: " + currentRound)
     console.log("Current Player score is: " + playerScore)
