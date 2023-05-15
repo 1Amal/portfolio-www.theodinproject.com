a441728dc157d56231dfafc755f851c4b1b1ebac
Amal K <12756149+1Amal@users.noreply.github.com>
2023-05-16 08:32:17 +1000

    Completed javascript-exercises-main/02_repeatString

--- javascript-exercises-main/02_repeatString/repeatString.js
+++ javascript-exercises-main/02_repeatString/repeatString.js
@@ -1,6 +1,22 @@
-const repeatString = function() {
+const repeatString = function(string,num) {
+    let returnString="";
+    if (num<0){
+        returnString="ERROR"; // If negative number output error
+              }
 
+    else      { //If positive number run below loop
+                for (let i=0; i<num; i++) // for loop to repeat until it reaches given number
+              {
+                returnString=returnString.concat(string); //Add string value after each loop iteration
+              }
+
+              }
+
+        
+     
+    return (returnString); // Return the final string
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
