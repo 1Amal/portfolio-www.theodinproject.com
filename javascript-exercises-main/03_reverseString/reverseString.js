35febc6568cd3b176faa6f156cec46ae70b552ae
Amal K <12756149+1Amal@users.noreply.github.com>
2023-05-17 08:06:15 +1000

    Completed the 03-Reverse-String excercise in JavaScript Excercise section

--- javascript-exercises-main/03_reverseString/reverseString.js
+++ javascript-exercises-main/03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(reverseThis) {
+// return ("olleh")
+var splitString=reverseThis.split(""); //This Line will split the string into an array
+var reversedString=splitString.reverse();// This line will reverse the array
+var joinedArray=reversedString.join(""); // This line will join the reversed array into an string
+return (joinedArray); //This will return final result to the function
 };
 
 // Do not edit below this line
