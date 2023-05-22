fcca870da132fa32ccf4809d39afc4c6663ceac2
Amal K <12756149+1Amal@users.noreply.github.com>
2023-05-22 21:59:32 +1000

    Completed 04_RemoveFromArray JavaScript Excercise

--- javascript-exercises-main/04_removeFromArray/removeFromArray.js
+++ javascript-exercises-main/04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(inputArray,...numbersToRemove) {
 
+const filteredArray=inputArray.filter(findNumber);
+
+function findNumber(numberReq) {
+return (!numbersToRemove.includes(numberReq));
+
+}
+
+    return (filteredArray);
 };
 
 // Do not edit below this line
