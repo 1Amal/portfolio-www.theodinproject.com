60371f82cf62b772f33b6a9fa16c8ba0ff8a00ac
Amal K <12756149+1Amal@users.noreply.github.com>
2023-05-27 15:57:16 +1000

    Finished 07_tempConversion JavaScript Excercises, This concludes all the excercises in Odin Project Fundamentals 4 Section

--- javascript-exercises-main/07_tempConversion/tempConversion.js
+++ javascript-exercises-main/07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+// Farenhit to Celsius
+// X Faranheit=(X-32)x5/9Centigrade
+const convertToCelsius = function(temperatureInFahrenheit) {
+let temperatureInCelsius = (temperatureInFahrenheit-32)*(5/9);
+return (Math.round(temperatureInCelsius*10)/10);
+
 };
 
-const convertToFahrenheit = function() {
+//Celsius to Farenheit
+// X Celsius ==((Xx(9/5))+32) Farenheit
+const convertToFahrenheit = function(temperatureInCelsius2) {
+let temperatureInFahrenheit2=((temperatureInCelsius2*(9/5))+32);
+return (Math.round(temperatureInFahrenheit2*10)/10);
+
 };
 
 // Do not edit below this line
