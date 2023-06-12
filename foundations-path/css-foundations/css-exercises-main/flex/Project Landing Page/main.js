39eb3d9fe6463cc74b215b07e10d354d50725cad
Amal Kariyawasam <12756149+1Amal@users.noreply.github.com>
2023-06-13 08:34:52 +1000

    Add completed FlexBox Landing page project files

---
+++ foundations-path/css-foundations/css-exercises-main/flex/Project Landing Page/main.js
@@ -0,0 +1,18 @@
+//Amal Kariyawasam, 2023
+
+/* This is the main JavaScript file for this portfolio website */
+
+function time_function()
+{
+    document.getElementById("time").textContent=Date();
+
+
+}
+
+function page_version()
+{
+    document.getElementById("version").textContent="Website updated on: 13/06/2023"
+}
+
+time_function()
+page_version()
