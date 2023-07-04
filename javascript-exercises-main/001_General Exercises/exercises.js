adca4a06fb54b42e9de76a3946129a60f65bb135
Amal Kariyawasam <12756149+1Amal@users.noreply.github.com>
2023-07-04 21:51:13 +1000

    Added Javascript general excercise section using  javascript file with Html document

---
+++ javascript-exercises-main/001_General Exercises/exercises.js
@@ -0,0 +1,10 @@
+//Use this file to do any short JavaScript exercises
+
+// Create an empty object user.
+let user=new Object();
+// Add the property name with the value John.
+// Add the property surname with the value Smith.
+// Change the value of the name to Pete.
+// Remove the property name from the object.
+
+ console.log(user)
