0a4452277de4e9b7923a9734f13bdcf9da77a2fb
Amal Kariyawasam <12756149+1Amal@users.noreply.github.com>
2023-08-11 08:07:55 +1000

    Started the Final Excecise 12_findTheOldest

--- javascript-exercises-main/001_JavaScript Test and Experiment/jsTest.js
+++ javascript-exercises-main/001_JavaScript Test and Experiment/jsTest.js
@@ -1,6 +1,28 @@
 /*Use this file to do any short JavaScript exercises and to experiment
 with new concepts*/
 
+const people = [
+  {
+    name: "Carly",
+    yearOfBirth: 1942,
+    yearOfDeath: 1970,
+  },
+  {
+    name: "Ray",
+    yearOfBirth: 1962,
+    yearOfDeath: 2011,
+  },
+  {
+    name: "Jane",
+    yearOfBirth: 1912,
+    yearOfDeath: 1941,
+  },
+]
+
+
+
+console.log(people[0].name);
+
 
 
 
