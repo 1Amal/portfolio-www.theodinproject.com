3523cd61ca6d47c2b4faaf83152a887c26211757
Amal K <12756149+1Amal@users.noreply.github.com>
2024-02-05 16:12:56 +1100

    Signup form challege- Link JavaScript File

---
+++ Full-Stack-JavaScript/Forms/Project_Signup_Form/formLogic.js
@@ -0,0 +1,10 @@
+console.log("Script enabled")
+
+function passwordCheck()
+{
+    const passwordCheck=document.querySelector('#confirmPassword');
+    passwordCheck.addEventListener('click',()=>{
+        console.log (passwordCheck);
+    })
+    
+}
