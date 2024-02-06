dd9b84e48b18524d06ed46f612d01b99981f635f
Amal K <12756149+1Amal@users.noreply.github.com>
2024-02-06 16:27:12 +1100

    Signup form challege:Add JavaScript code to check password inputs are the same

--- Full-Stack-JavaScript/Forms/Project_Signup_Form/formLogic.js
+++ Full-Stack-JavaScript/Forms/Project_Signup_Form/formLogic.js
@@ -1,10 +1,34 @@
-console.log("Script enabled")
+let password1Check;
+let password2Check;
 
-function passwordCheck()
-{
-    const passwordCheck=document.querySelector('#confirmPassword');
-    passwordCheck.addEventListener('click',()=>{
-        console.log (passwordCheck);
-    })
-    
-}
+const password1=document.querySelector("#password1");
+const password2=document.querySelector("#password2");
+const passwordStatus=document.querySelector("#passwordStatus");
+
+document.addEventListener("DOMContentLoaded", function() {
+    var submitButton=document.querySelector("#createButton")
+
+    submitButton.addEventListener("click", function() {
+
+        let passwordCompare=password1.value==password2.value;
+        console.log(passwordCompare);
+
+        if (passwordCompare===true)
+            {
+                passwordStatus.textContent="Passwords matched"; 
+                passwordStatus.style.color="#89c851";
+                password1.style.background="#89c851";
+                password2.style.background="#89c851";
+            }
+        
+        else if (passwordCompare===false)
+        {
+            passwordStatus.textContent="Passwords don't match";
+            passwordStatus.style.color="red";
+            password1.style.background="#ef1616";
+            password2.style.background="#ef1616";
+        }
+        
+    });
+});
+
