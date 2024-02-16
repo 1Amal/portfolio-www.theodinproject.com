1e7e706c1e630566a82bfd04ecf2be8ffdbcb4ff
Amal K <12756149+1Amal@users.noreply.github.com>
2024-02-16 16:59:38 +1100

    Completed MDN Javascript refresh excercises

---
+++ Full-Stack-JavaScript/JavaScript/beginner-html-site-styled-gh-pages/scripts/main.js
@@ -0,0 +1,28 @@
+const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/firefox-icon.png") {
+    myImage.setAttribute("src", "images/firefox2.png");
+  } else {
+    myImage.setAttribute("src", "images/firefox-icon.png");
+  }
+};
+
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+  const myName = prompt("Please enter your name.");
+  if (!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `Mozilla is cool, ${myName}`;
+  }
+}
+
+  myButton.onclick = () => {
+    setUserName();
+  };
