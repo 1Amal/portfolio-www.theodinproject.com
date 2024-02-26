dd019c3cc87b0d73b9c9966401bce6e56d498820
Amal K <12756149+1Amal@users.noreply.github.com>
2024-02-26 16:46:26 +1100

    Create Code Challeneg-Library and insert the initial files

---
+++ Full-Stack-JavaScript/JavaScript/codeChallenge_Library/logic.js
@@ -0,0 +1,13 @@
+const myLibrary=[];
+
+// author, title, number of pages, whether it’s been read and anything else you might want
+
+function bookInfoDisplay()
+{
+    const bookInfoDiv=document.querySelector(".bookInfo");
+    // const content=document.createElement("p");
+    // content.classList.add("content");
+    // content.textContent="Hello";
+}
+
+bookInfoDisplay();
