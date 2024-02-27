8a7aecdbbb1435b180d3a8b800aadebf327f7cc6
Amal K <12756149+1Amal@users.noreply.github.com>
2024-02-28 10:47:57 +1100

    Code Challenge: Library, Add conde to create Book objects

--- Full-Stack-JavaScript/JavaScript/codeChallenge_Library/logic.js
+++ Full-Stack-JavaScript/JavaScript/codeChallenge_Library/logic.js
@@ -2,12 +2,31 @@ const myLibrary=[];
 
 // author, title, number of pages, whether it’s been read and anything else you might want
 
-function bookInfoDisplay()
+function bookInfoDisplay(displayOut)
 {
     const bookInfoDiv=document.querySelector(".bookInfo");
     // const content=document.createElement("p");
     // content.classList.add("content");
-    // content.textContent="Hello";
+    bookInfoDiv.textContent=displayOut;
 }
 
-bookInfoDisplay();
+
+
+function bookData(author, title, numberOfPages,read)
+{
+    this.author=author;
+    this.title=title;
+    this.numberOfPages=numberOfPages;
+    this.read=read;
+}
+
+const book1=new bookData("Amal","The Great",4939,"No");
+
+// bookInfoDisplay(book1['author']); 
+
+function addBookToLibrary() {
+    myLibrary.push(book1.title);
+    bookInfoDisplay(myLibrary[0]);
+  }
+
+  addBookToLibrary();
