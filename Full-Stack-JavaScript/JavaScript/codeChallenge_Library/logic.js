808bffcf00f745a1e83ddbe142fa7ac0edb11915
Amal K <12756149+1Amal@users.noreply.github.com>
2024-02-28 18:03:14 +1100

    Code Challenge - Library: Added code display the myLibrary array in the webpage and added pending tasks as a comment in Js file

--- Full-Stack-JavaScript/JavaScript/codeChallenge_Library/logic.js
+++ Full-Stack-JavaScript/JavaScript/codeChallenge_Library/logic.js
@@ -7,6 +7,15 @@
 6) Each book "book info Card" will have a button "Read" which will update the relevant object "book.read=yes". Then call up the function "bookInfoDisplay" to update the webpage. 
 */
 
+/* Pending Tasks
+1) For "addBookToLibrary" enable code to receive four variables that will be converted to a object using "bookData" object constructor.
+2) Add HTML input form with buttons
+3) Enable "addBookToLibrary" to recived four variables from the HTML code
+4) Add a "Delete" button and "Read" button
+5) Write code so "Delete" button deletes book from the array
+6) Write code so "Read" button change the status i.e. if current read=yes then change to read=no, vice versa
+*/
+
 
 const myLibrary=[];
 
@@ -15,53 +24,47 @@ const bookInfoTitle=document.querySelector(".bookInfoTitle");
 const bookInfoNumberOfPages=document.querySelector(".bookInfoNumberOfPages");
 const bookInfoRead=document.querySelector(".bookInfoRead");
 
+//Code for creating New Book Object
+function bookData(author, title, numberOfPages,read)
+{
+this.author=author;
+this.title=title;
+this.numberOfPages=numberOfPages;
+this.read=read;
+}
 
-
+//Code to add the new book details object into the myLibrary array
 function addBookToLibrary(newBook) 
     {
         myLibrary.push(new bookData("Amal","The Great",4939,"No"));
         myLibrary.push(new bookData("Wimal","Not So Great",423,"No"));
         myLibrary.push(new bookData("Monica","Not tooo bad",223,"Yes"));
+        myLibrary.push(new bookData("Siripala","Gamaya",2232,"No"));
+
         bookInfoDisplay(myLibrary);
         console.log(myLibrary);
         
     }
 
 
-  function bookData(author, title, numberOfPages,read)
-    {
-    this.author=author;
-    this.title=title;
-    this.numberOfPages=numberOfPages;
-    this.read=read;
-    }
 
-
-function bookInfoDisplay(displayOut)
+function bookInfoDisplay(myLibrary)
     {
-    // const bookInfoDiv=document.querySelector(".bookInfo");
-    // const content=document.createElement("p");
-    // content.classList.add("content");
+
 
     for (let i=0;i<myLibrary.length;i++)
     {
-        const card=document.querySelector(".card");
+        const LibraryStatusDiv=document.querySelector(".LibraryStatusDiv");
         
         const newDiv=document.createElement('div');
 
-        // bookInfoAuthor.textContent="Author Name: " + myLibrary[i]["author"];
-
-        newDiv.textContent="Author Name: " + myLibrary[i]["author"] + " " +
-                            "Title Name: " + myLibrary[i]["title"] + " "  +
-                            "Number of Pages: " + myLibrary[i]["numberOfPages"] + " " +
+        newDiv.textContent="Author Name: " + myLibrary[i]["author"] + " | " +
+                            "Title Name: " + myLibrary[i]["title"] + " | "  +
+                            "Number of Pages: " + myLibrary[i]["numberOfPages"] + " | " +
                             "Book Read: " + myLibrary[i]["read"];
-        card.appendChild(newDiv);
-
-        // bookInfoTitle.textContent="Title Name: " + myLibrary[i]["title"];
-    
-        // bookInfoNumberOfPages.textContent="Number of Pages: " + myLibrary[i]["numberOfPages"];
-    
-        // bookInfoRead.textContent="Book Read: " + myLibrary[i]["read"];
+        
+        newDiv.classList.add("bookDetails");
+        LibraryStatusDiv.appendChild(newDiv);
 
         console.log(myLibrary[i]);
 
@@ -69,10 +72,7 @@ function bookInfoDisplay(displayOut)
 
 
 
-
-
     }
 
-// bookInfoDisplay(book1['author']); 
 
   addBookToLibrary();
