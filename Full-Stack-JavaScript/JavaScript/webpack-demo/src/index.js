cc5428b1cf0fcaef735ce2126c98b32e8613477a
Amal K <12756149+1Amal@users.noreply.github.com>
2024-04-05 16:20:11 +1100

    Completed WebPack Production tutorial

--- Full-Stack-JavaScript/JavaScript/webpack-demo/src/index.js
+++ Full-Stack-JavaScript/JavaScript/webpack-demo/src/index.js
@@ -1,18 +1,18 @@
-import _ from 'lodash';
-import printMe from './print.js';
+import { cube } from './math.js';
 
- function component() {
-   const element = document.createElement('div');
-  const btn = document.createElement('button');
-
-   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  btn.innerHTML = 'Click me and check the console!';
-  btn.onclick = printMe;
-
-  element.appendChild(btn);
-
-   return element;
+ if (process.env.NODE_ENV !== 'production') {
+   console.log('Looks like we are in development mode!');
  }
 
- document.body.appendChild(component());
+  function component() {
+    const element = document.createElement('pre');
+
+    element.innerHTML = [
+      'Hello webpack!',
+      '5 cubed is equal to ' + cube(5)
+    ].join('\n\n');
+
+    return element;
+  }
+
+  document.body.appendChild(component());
