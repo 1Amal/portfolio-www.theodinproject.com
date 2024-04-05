411659079ae1215d91fe0f745396fed4017e131c
Amal K <12756149+1Amal@users.noreply.github.com>
2024-04-05 15:44:37 +1100

    Completed the tutorial WebPack Development

--- Full-Stack-JavaScript/JavaScript/webpack-demo/webpack.config.js
+++ Full-Stack-JavaScript/JavaScript/webpack-demo/webpack.config.js
@@ -1,19 +1,25 @@
 const path = require('path');
- const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
- module.exports = {
-   entry: {
-     index: './src/index.js',
-     print: './src/print.js',
-   },
-   plugins: [
-     new HtmlWebpackPlugin({
-       title: 'Output Management',
-     }),
-   ],
-   output: {
-     filename: '[name].bundle.js',
-     path: path.resolve(__dirname, 'dist'),
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: './src/index.js',
+    print: './src/print.js',
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Development',
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
     clean: true,
-   },
- };
+   publicPath: '/',
+  },
+};
