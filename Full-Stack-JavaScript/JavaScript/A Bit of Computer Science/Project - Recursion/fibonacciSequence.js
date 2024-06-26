ccb3d43b0aa35da0ad48b358597c321a1fa40700
Amal K <12756149+1Amal@users.noreply.github.com>
2024-06-26 11:15:05 +1000

    Completed Fibonacci using iterative method

--- Full-Stack-JavaScript/JavaScript/A Bit of Computer Science/Project - Recursion/fibonacciSequence.js
+++ Full-Stack-JavaScript/JavaScript/A Bit of Computer Science/Project - Recursion/fibonacciSequence.js
@@ -4,17 +4,24 @@ function fibs (num)
     console.log("Hello Node, This is Fibonacci !");
 
     // F(n)=F(n-1)+F(n-2)
+    let previousNum1=0;
+    let previousNum2=1;
+    let currentNum;
+    const fibonacciArray=[0,1];
 
-    for (let i=0; i<num; i++)
+    for (let i=2; i<num; i++)
         {
-            const fibonacciArray=[];
-            let fibonacci=(num-1)+(num-2);
-            fibonacciArray.push(fibonacci);
-            console.log(fibonacciArray);
+
+            currentNum=previousNum1+previousNum2;
+            previousNum1=previousNum2;
+            previousNum2=currentNum;
+            fibonacciArray.push(currentNum);
+
+            
         }
 
 
-
+        console.log(fibonacciArray);
 }
 
 fibs (8);
