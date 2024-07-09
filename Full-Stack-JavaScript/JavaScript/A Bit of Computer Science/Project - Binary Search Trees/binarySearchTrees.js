7b719659b9adfc46866d321c9ee32f68a670f5fb
Amal K <12756149+1Amal@users.noreply.github.com>
2024-07-09 16:32:48 +1000

    Project - Binary search trees, initial commit

---
+++ Full-Stack-JavaScript/JavaScript/A Bit of Computer Science/Project - Binary Search Trees/binarySearchTrees.js
@@ -0,0 +1,24 @@
+// 2024, Amal Kariyawasam
+
+class Node{
+    constructor(data)
+    {
+        this.data=data;
+        this.left;
+        this.right;
+    }
+}
+
+
+class Tree {
+    constructor(inputArray, root)
+    {
+        this.inputArray=inputArray;
+        this.root=root;
+    }
+}
+
+function buildTree(array)
+{
+    
+}
