00014330f94a3b0d0fd28981ad17d2bc6bfb775f
Amal Kariyawasam <12756149+1Amal@users.noreply.github.com>
2024-07-26 11:06:33 +1000

    Updated course progress index.html

--- Full-Stack-JavaScript/JavaScript/Testing JavaScript/analyzeArray.test.js
+++ Full-Stack-JavaScript/JavaScript/Testing JavaScript/analyzeArray.test.js
@@ -3,22 +3,21 @@ const newIstanceAnalyzeArray = new analyzeArray([1, 8, 3, 4, 2, 6]);
 const returnedObject = { average: 4, min: 1, max: 8, length: 6 };
 
 test("An analyzeArray function that takes an array of numbers and returns an object with the following properties: average, min, max, and length.", () => {
-  
-    expect(newIstanceAnalyzeArray.returnObject()).toStrictEqual(returnedObject);
+  expect(newIstanceAnalyzeArray.returnObject()).toStrictEqual(returnedObject);
 });
 
-test('Check for average', ()=>{
+test("Check for average", () => {
   expect(newIstanceAnalyzeArray.average()).toBe(4);
 });
 
-test('Check for Minimum',()=>{
+test("Check for Minimum", () => {
   expect(newIstanceAnalyzeArray.min()).toBe(1);
 });
 
-test('Check for Maximum',()=>{
+test("Check for Maximum", () => {
   expect(newIstanceAnalyzeArray.max()).toBe(8);
 });
 
-test('Check for Length',()=>{
+test("Check for Length", () => {
   expect(newIstanceAnalyzeArray.length()).toBe(6);
 });
