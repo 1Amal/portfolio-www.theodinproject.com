5d41bdead861c68bb6d5e011a4d7b7b4dfc16e01
Amal Kariyawasam <12756149+1Amal@users.noreply.github.com>
2024-08-16 20:59:18 +1000

    Finished Examples in CSS Animations lesson

--- Full-Stack-JavaScript/CSS/Animations/logic.js
+++ Full-Stack-JavaScript/CSS/Animations/logic.js
@@ -1,10 +1,21 @@
 function change() {
-    const elements = document.querySelectorAll("div.box");
-    for (const element of elements) {
-      element.classList.toggle("transformed-state");
-    }
+  const elements = document.querySelectorAll("div.box");
+  for (const element of elements) {
+    element.classList.toggle("transformed-state");
   }
-  
-  const changeButton = document.querySelector("#change");
-  changeButton.addEventListener("click", change);
-  
+}
+
+const changeButton = document.querySelector("#change");
+changeButton.addEventListener("click", change);
+
+//Move the ball
+
+const f = document.getElementById("foo");
+document.addEventListener(
+  "click",
+  (ev) => {
+    f.style.transform = `translateY(${ev.clientY - 25}px)`;
+    f.style.transform += `translateX(${ev.clientX - 25}px)`;
+  },
+  false
+);
