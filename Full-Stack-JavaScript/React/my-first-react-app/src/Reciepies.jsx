f3fea8ed456d0e48d6e9179132032f43131cb6d4
Amal Kariyawasam <12756149+1Amal@users.noreply.github.com>
2024-09-09 13:13:15 +1000

    Completed Lesson 'React: Rendering Techniques'

--- Full-Stack-JavaScript/React/my-first-react-app/src/Reciepies.jsx
+++ Full-Stack-JavaScript/React/my-first-react-app/src/Reciepies.jsx
@@ -1,30 +1,43 @@
-export const recipes = [{
-    id: 'greek-salad',
-    name: 'Greek Salad',
-    ingredients: ['tomatoes', 'cucumber', 'onion', 'olives', 'feta']
-  }, {
-    id: 'hawaiian-pizza',
-    name: 'Hawaiian Pizza',
-    ingredients: ['pizza crust', 'pizza sauce', 'mozzarella', 'ham', 'pineapple']
-  }, {
-    id: 'hummus',
-    name: 'Hummus',
-    ingredients: ['chickpeas', 'olive oil', 'garlic cloves', 'lemon', 'tahini']
-  }];
-  
-const recipesDisplay=recipes.map(recipeName=>
-
-);
+export const recipes = [
+  {
+    id: "greek-salad",
+    name: "Greek Salad",
+    ingredients: ["tomatoes", "cucumber", "onion", "olives", "feta"],
+  },
+  {
+    id: "hawaiian-pizza",
+    name: "Hawaiian Pizza",
+    ingredients: [
+      "pizza crust",
+      "pizza sauce",
+      "mozzarella",
+      "ham",
+      "pineapple",
+    ],
+  },
+  {
+    id: "hummus",
+    name: "Hummus",
+    ingredients: ["chickpeas", "olive oil", "garlic cloves", "lemon", "tahini"],
+  },
+];
 
 export default function RecipeList() {
-  return (
-    <div>
-      <h1>Recipes</h1>
-    <div>
-        <h2>{recipes[0].name}</h2>
-        
-        <ul>{recipes[0].ingredients}</ul>
-    </div>
-    </div>
-  );
-}
+    return (
+      <div>
+        <h1>Recipes</h1>
+        {recipes.map(recipe =>
+          <div key={recipe.id}>
+            <h2>{recipe.name}</h2>
+            <ul>
+              {recipe.ingredients.map(ingredient =>
+                <li key={ingredient}>
+                  {ingredient}
+                </li>
+              )}
+            </ul>
+          </div>
+        )}
+      </div>
+    );
+  }
