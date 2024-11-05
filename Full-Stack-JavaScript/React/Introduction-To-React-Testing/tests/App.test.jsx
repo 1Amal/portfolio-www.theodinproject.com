5915ff47f90232481452137f56cd8dfa944c2c69
Amal Kariyawasam <12756149+1Amal@users.noreply.github.com>
2024-11-05 13:23:50 +1100

    Introduction To React Testing: Setup Vitest

--- Full-Stack-JavaScript/React/Introduction-To-React-Testing/tests/App.test.jsx
+++ Full-Stack-JavaScript/React/Introduction-To-React-Testing/tests/App.test.jsx
@@ -11,16 +11,62 @@
 // });
 
 
-import { render, screen } from '@testing-library/react';
+// import { render, screen } from '@testing-library/react';
 
-import App from '../src/App';
+// import App from '../src/App';
 
-describe('App', () => {
-  it('renders headline', () => {
-    render(<App title="React" />);
+// describe('App', () => {
+//   it('renders headline', () => {
+//     render(<App title="React" />);
 
-    screen.debug();
+//     screen.debug();
 
-    // check if App components renders headline
+//     // check if App components renders headline
+//   });
+// });
+
+// App.test.jsx
+
+// import { describe, it, expect } from "vitest";
+// import { render, screen } from "@testing-library/react";
+// import App from "../src/App";
+
+// describe("App component", () => {
+//   it("renders correct heading", () => {
+//     render(<App />);
+//     // expect(screen.getByRole("heading").textContent).toMatch(/our first test/i);
+//     expect(screen.getByRole("heading",{name:"Our First Test"}));
+
+//   });
+// });
+
+// App.test.jsx
+
+import { describe, it, expect } from "vitest";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import App from "../src/App";
+
+describe("App component", () => {
+  it("renders magnificent monkeys", () => {
+    // since screen does not have the container property, we'll destructure render to obtain a container for this test
+    const { container } = render(<App />);
+    expect(container).toMatchSnapshot();
+    // expect(container).toMatchInlineSnapshot('"FOOBAR"');
+  });
+
+  it("renders radical rhinos after button click", async () => {
+    const user = userEvent.setup();
+
+    render(<App />);
+    const button = screen.getByRole("button", { name: "Click Me" });
+
+    await user.click(button);
+
+    expect(screen.getByRole("heading").textContent).toMatch(/radical rhinos/i);
   });
 });
+
+
+
+
