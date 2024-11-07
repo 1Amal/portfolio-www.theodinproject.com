52eea4fcb02bb867582c4bb4dbecfa0c69332a27
Amal Kariyawasam <12756149+1Amal@users.noreply.github.com>
2024-11-08 10:18:15 +1100

    Finished Lesson: Type Checking With PropTypes

--- Full-Stack-JavaScript/React/reactPlayGround/LessonTypeCheckingWithPropTypes/TypeCheckingProps.jsx
+++ Full-Stack-JavaScript/React/reactPlayGround/LessonTypeCheckingWithPropTypes/TypeCheckingProps.jsx
@@ -1,12 +1,18 @@
 import PropTypes from 'prop-types';
+import '../src/style.css';
 
 const RenderName = (props) => {
-  return <div>{props.name}</div>;
+  return <div><h1>Lesson: Type Checking With PropTypes</h1>
+    <h1>{props.name}</h1></div>;
   
 };
 
 RenderName.propTypes = {
-  name: PropTypes.string,
+  name: PropTypes.string.isRequired,
+};
+
+RenderName.defaultProps = {
+  name: 'Amal',
 };
 
 export default RenderName;
