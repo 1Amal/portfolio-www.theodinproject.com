a72f3e0607cb802eb37b117397be1acb60d342c3
Amal Kariyawasam <12756149+1Amal@users.noreply.github.com>
2024-12-10 16:44:09 +1100

    Add default path to index.html

--- Full-Stack-JavaScript/NodeJs/Project_Basic_Information_Site/index.js
+++ Full-Stack-JavaScript/NodeJs/Project_Basic_Information_Site/index.js
@@ -1,6 +1,34 @@
-var http = require('http');
+let http = require("http");
+let fs = require("fs");
+let url = require("url");
 
-http.createServer(function(req,res){
-    res.write('Welcome to informational site');
-    res.end();
-}).listen(8080)
+http
+  .createServer(function (req, res) {
+    let q = url.parse(req.url, true);
+    let fileName = "." + q.pathname + ".html";
+    console.log(q.pathname);
+
+    if (q.pathname === "/") {
+      fs.readFile("index.html", function (err, data) {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write(data);
+        return res.end();
+      });
+    } else {
+      fs.readFile(fileName, function (err, data) {
+        if (err) {
+          res.writeHead(404, { "Content-Type": "text/html" });
+          res.write("Hey");
+          return res.end("404 Not Found");
+        } else {
+          res.writeHead(200, { "Content-Type": "text/html" });
+          res.write(data);
+          console.dir("Server Accessed from Browser");
+          return res.end();
+        }
+      });
+    }
+  })
+  .listen(8080);
+
+console.log("Server Running on http://localhost:8080/");
