20d781236e0d78268dda59d56a067cf010431b68
Amal Kariyawasam <12756149+1Amal@users.noreply.github.com>
2024-12-16 12:24:28 +1100

    Finished NodeJs Views Lesson Exercises

--- Full-Stack-JavaScript/NodeJs/Views/app.js
+++ Full-Stack-JavaScript/NodeJs/Views/app.js
@@ -1,14 +1,25 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
+const links = [
+  { href: "/", text: "Home" },
+  { href: "about", text: "About" },
+];
+
+const users = ["Rose", "Cake", "Biff"];
 
 const path = require("node:path");
+
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.send("NodeJs Views Lesson");
+  //   res.send("NodeJs Views Lesson");
+  res.render("index", { links: links, users: users });
+  // console.log(message)
 });
 
 app.listen(PORT, () => {
