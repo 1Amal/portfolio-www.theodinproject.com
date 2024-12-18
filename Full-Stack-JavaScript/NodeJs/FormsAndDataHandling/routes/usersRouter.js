458ca716c0ec0546bca3e3828ce8e033c54af0f9
Amal Kariyawasam <12756149+1Amal@users.noreply.github.com>
2024-12-18 18:03:27 +1100

    Forms and Data Handling completed lesson tutorial

--- Full-Stack-JavaScript/NodeJs/FormsAndDataHandling/routes/usersRouter.js
+++ Full-Stack-JavaScript/NodeJs/FormsAndDataHandling/routes/usersRouter.js
@@ -7,4 +7,9 @@ usersRouter.get("/", usersController.usersListGet);
 usersRouter.get("/create", usersController.usersCreateGet);
 usersRouter.post("/create", usersController.usersCreatePost);
 
+usersRouter.get("/:id/update", usersController.usersUpdateGet);
+usersRouter.post("/:id/update", usersController.usersUpdatePost);
+
+usersRouter.post("/:id/delete", usersController.usersDeletePost);
+
 module.exports = usersRouter;
