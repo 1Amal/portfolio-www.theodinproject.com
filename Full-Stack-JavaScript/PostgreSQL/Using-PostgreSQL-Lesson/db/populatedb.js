f73bde3dde25365b4fea73d3247ea02059232c0f
Amal Kariyawasam <12756149+1Amal@users.noreply.github.com>
2024-12-23 11:51:55 +1100

    Add script for automated database creation

---
+++ Full-Stack-JavaScript/PostgreSQL/Using-PostgreSQL-Lesson/db/populatedb.js
@@ -0,0 +1,31 @@
+#! /usr/bin/env node
+
+const { Client } = require("pg");
+
+require("dotenv").config();
+
+const SQL = `
+CREATE TABLE IF NOT EXISTS usernames (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  username VARCHAR ( 255 )
+);
+
+INSERT INTO usernames (username) 
+VALUES
+  ('Bryan'),
+  ('Odin'),
+  ('Damon');
+`;
+
+async function main() {
+  console.log("seeding...");
+  const client = new Client({
+    connectionString: `postgresql://${process.env.USER}:${process.env.PW}@localhost:5432/top_users`,
+  });
+  await client.connect();
+  await client.query(SQL);
+  await client.end();
+  console.log("done");
+}
+
+main();
