8acc26c568fa698f580b5cb9edfeed11e812c467
Amal Kariyawasam <12756149+1Amal@users.noreply.github.com>
2025-01-07 10:36:34 +1100

    Add lesson files for Authentication Basics

---
+++ Full-Stack-JavaScript/NodeJs/AuthenticationBasics/app.js
@@ -0,0 +1,24 @@
+/////// app.js
+
+const path = require("node:path");
+const { Pool } = require("pg");
+const express = require("express");
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require('passport-local').Strategy;
+
+const pool = new Pool({
+  // add your configuration
+});
+
+const app = express();
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(session({ secret: "cats", resave: false, saveUninitialized: false }));
+app.use(passport.session());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => res.render("index"));
+
+app.listen(3000, () => console.log("app listening on port 3000!"));
