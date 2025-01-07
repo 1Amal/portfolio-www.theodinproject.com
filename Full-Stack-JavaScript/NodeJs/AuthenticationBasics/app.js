3e967290121cc1ee585e8c68d6d18968f8c33da4
Amal Kariyawasam <12756149+1Amal@users.noreply.github.com>
2025-01-07 13:21:47 +1100

    Added functionality to login and logout

--- Full-Stack-JavaScript/NodeJs/AuthenticationBasics/app.js
+++ Full-Stack-JavaScript/NodeJs/AuthenticationBasics/app.js
@@ -15,7 +15,7 @@ const pool = new Pool({
   database: process.env.DATABASE_NAME,
   password: process.env.INVENTORY_DATABASE_PASSWORD,
   port: process.env.DATABASE_PORT,
-  ssl:false
+  ssl: false,
 });
 
 const app = express();
@@ -26,7 +26,9 @@ app.use(session({ secret: "cats", resave: false, saveUninitialized: false }));
 app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => res.render("index"));
+app.get("/", (req, res) => {
+  res.render("index", { user: req.user });
+});
 
 app.get("/sign-up", (req, res) => res.render("sign-up-form"));
 
@@ -39,6 +41,62 @@ app.post("/sign-up", async (req, res, next) => {
     res.redirect("/");
   } catch (err) {
     return next(err);
+  }
+});
+
+app.post(
+  "/log-in",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/",
+  })
+);
+
+app.get("/log-out", (req, res, next) => {
+  req.logout((err) => {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
+  });
+});
+
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const { rows } = await pool.query(
+        "SELECT * FROM users WHERE username = $1",
+        [username]
+      );
+      const user = rows[0];
+
+      if (!user) {
+        return done(null, false, { message: "Incorrect username" });
+      }
+      if (user.password !== password) {
+        return done(null, false, { message: "Incorrect password" });
+      }
+      return done(null, user);
+    } catch (err) {
+      return done(err);
+    }
+  })
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const { rows } = await pool.query("SELECT * FROM users WHERE id = $1", [
+      id,
+    ]);
+    const user = rows[0];
+
+    done(null, user);
+  } catch (err) {
+    done(err);
   }
 });
 
