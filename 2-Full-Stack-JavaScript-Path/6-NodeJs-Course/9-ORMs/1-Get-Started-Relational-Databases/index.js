845ebefeb54dd09f1a244d82861e9cd699193fe5
Amal Kariyawasam <12756149+1Amal@users.noreply.github.com>
2025-01-16 13:30:21 +1100

    Completed get started guide : relational databases Completed get started guide : relational databases which covers migrations, schema, and the Prisma client.

--- 2-Full-Stack-JavaScript-Path/6-NodeJs-Course/9-ORMs/1-Get-Started-Relational-Databases/index.js
+++ 2-Full-Stack-JavaScript-Path/6-NodeJs-Course/9-ORMs/1-Get-Started-Relational-Databases/index.js
@@ -1,20 +1,52 @@
-const { PrismaClient } = require('@prisma/client')
+const { PrismaClient } = require("@prisma/client");
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
+
+// async function main() {
+//   const allUsers = await prisma.user.findMany()
+//   console.log(allUsers)
+// }
+
+// async function main() {
+//   await prisma.user.create({
+//     data: {
+//       name: 'Alice',
+//       email: 'alice@prisma.io',
+//       posts: {
+//         create: { title: 'Hello World' },
+//       },
+//       profile: {
+//         create: { bio: 'I like turtles' },
+//       },
+//     },
+//   })
+
+//   const allUsers = await prisma.user.findMany({
+//     include: {
+//       posts: true,
+//       profile: true,
+//     },
+//   })
+//   console.dir(allUsers, { depth: null })
+// }
 
 async function main() {
-  // ... you will write your Prisma Client queries here
-  
-  const allUsers = await prisma.user.findMany()
-  console.log(allUsers)
+  const post = await prisma.post.update({
+    where: { id: 1 },
+    data: { published: true },
+  });
+
+  const allUsers = await prisma.post.findMany();
+  console.log(allUsers);
+  console.log(post);
 }
 
 main()
   .then(async () => {
-    await prisma.$disconnect()
+    await prisma.$disconnect();
   })
   .catch(async (e) => {
-    console.error(e)
-    await prisma.$disconnect()
-    process.exit(1)
-  })
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
