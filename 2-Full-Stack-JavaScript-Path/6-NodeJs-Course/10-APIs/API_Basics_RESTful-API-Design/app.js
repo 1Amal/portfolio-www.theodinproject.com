101cf54ff78e10a4266669568561f5eebcea4b58
Amal Kariyawasam <12756149+1Amal@users.noreply.github.com>
2025-02-04 11:19:02 +1100

    Created End point paths

--- 2-Full-Stack-JavaScript-Path/6-NodeJs-Course/10-APIs/API_Basics_RESTful-API-Design/app.js
+++ 2-Full-Stack-JavaScript-Path/6-NodeJs-Course/10-APIs/API_Basics_RESTful-API-Design/app.js
@@ -1,12 +1,32 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
-app.post('/', (req, res) => {
+app.get('/articles', (req, res) => {
+  const articles = [];
+  // code to retrieve an article...
+  res.json(articles);
+});
+
+app.post('/articles', (req, res) => {
+  // code to add a new article...
   res.json(req.body);
+});
+
+app.put('/articles/:id', (req, res) => {
+  const { id } = req.params;
+  // code to update an article...
+  res.json(req.body);
+});
+
+app.delete('/articles/:id', (req, res) => {
+  const { id } = req.params;
+  // code to delete an article...
+  res.json({ deleted: id });
 });
 
 app.listen(3000, () => console.log('server started'));
